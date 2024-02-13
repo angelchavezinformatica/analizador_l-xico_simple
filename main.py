0ac325bf77e97954bc05292bb002186f388828cb
angelchavezinformatica <156960915+angelchavezinformatica@users.noreply.github.com>
2024-02-12 19:59:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from core import ScannerOperations
+from core import ScannerOperations, ScannerOperationsInvalid
 
 if __name__ == '__main__':
     scanner = ScannerOperations()
+
     while True:
         inpt = input("Ingrese una expresión o enter: ")
+
         if inpt == "":
             break
-        if scanner.scanner(string=inpt):
-            print("Expresión válida")
-        else:
+
+        try:
+            for _ in scanner.scanner(string=inpt):
+                pass
+        except ScannerOperationsInvalid:
             print("Expresión inválida")
+            continue
+        print("Expresión válida")
