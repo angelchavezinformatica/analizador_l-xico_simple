a3c231dd5b11f33d86bd719d710bdc5e4040f2ce
angelchavezinformatica <156960915+angelchavezinformatica@users.noreply.github.com>
2024-02-12 19:58:38 -0500

    Update scanner_number.py

--- core/scanner_number.py
+++ core/scanner_number.py
@@ -4,44 +4,28 @@ from .errors import ScannerNumberInvalid
 class ScannerNumber:
     def __init__(self) -> None:
         self.numbers = "0123456789"
+        self.signs = '+-'
+        self.dot = '.'
 
     def scanner(self, string: str) -> None:
         state = 0
-        lenght = len(string)
 
-        for i in range(lenght):
-            char = string[i]
-
-            if state == 0:
-                if char in '+-':
-                    state = 1
-                elif char in self.numbers:
-                    state = 2
-                else:
-                    raise ScannerNumberInvalid()
-            elif state == 1:
-                if char in self.numbers:
-                    state = 2
-                else:
-                    raise ScannerNumberInvalid()
-            elif state == 2:
-                if char in self.numbers:
-                    state = 2
-                elif char == '.':
-                    state = 3
-                else:
-                    raise ScannerNumberInvalid()
-            elif state == 3:
-                if char in self.numbers:
-                    state = 4
-                else:
-                    raise ScannerNumberInvalid()
-            elif state == 4:
-                if char in self.numbers:
-                    state = 4
-                else:
-                    raise ScannerNumberInvalid()
-        else:
-            if state in (2, 4):
-                return
-        raise ScannerNumberInvalid()
+        for char in string:
+            if state == 0 and char in self.signs:
+                state = 1
+            elif state == 0 and char in self.numbers:
+                state = 2
+            elif state == 1 and char in self.numbers:
+                state = 2
+            elif state == 2 and char in self.numbers:
+                continue
+            elif state == 2 and char in self.dot:
+                state = 3
+            elif state == 3 and char in self.numbers:
+                state = 4
+            elif state == 4 and char in self.numbers:
+                continue
+            else:
+                raise ScannerNumberInvalid()
+        if not (state == 2 or state == 4):
+            raise ScannerNumberInvalid()
