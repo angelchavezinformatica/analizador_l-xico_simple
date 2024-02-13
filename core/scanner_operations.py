ea8a72a6180d413d7166d53613d017c1386925ef
angelchavezinformatica <156960915+angelchavezinformatica@users.noreply.github.com>
2024-02-12 19:58:58 -0500

    Update scanner_operations.py

--- core/scanner_operations.py
+++ core/scanner_operations.py
@@ -1,37 +1,35 @@
 import re
 
-from typing import List
-
 from core.scanner_number import ScannerNumber
 from .errors import ScannerNumberInvalid, ScannerOperationsInvalid
-
-ListStr = List[str]
 
 
 class ScannerOperations:
     def __init__(self) -> None:
-        self.operations = "+-*/"
+        self.operators = "+-*/"
 
-    def scanner(self, string: str) -> None:
+    def scanner(self, string: str):
         scaner_number = ScannerNumber()
         state = 0
 
         try:
-            for st in self._split_str(string=string):
+            for token in self._split_str(string=string):
                 if state == 0:
-                    scaner_number.scanner(st)
+                    scaner_number.scanner(string=token)
                     state = 1
-                elif state == 1:
-                    if (len(st) == 1) and (st in self.operations):
-                        state = 0
+                elif state == 1 and token in self.operators:
+                    state = 0
                 else:
                     raise ScannerOperationsInvalid()
-        except ScannerNumberInvalid or ScannerOperationsInvalid:
-            return False
-        else:
-            if state == 1:
-                return True
-        return False
+                yield token
+        except ScannerNumberInvalid:
+            raise ScannerOperationsInvalid()
+        if state == 1:
+            return
+        raise ScannerOperationsInvalid()
 
-    def _split_str(self, string: str) -> ListStr:
-        return re.sub(pattern=r'\s+', repl=' ', string=string).split(" ")
+    def _split_str(self, string: str):
+        tokens = re.sub(pattern=r'\s+', repl=' ',
+                        string=string).strip().split(" ")
+        for token in tokens:
+            yield token
